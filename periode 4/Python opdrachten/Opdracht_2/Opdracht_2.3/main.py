c802b148202a51117c4a9b1fe6d29b76dfb19946
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2023-06-20 10:38:29 +0200

    Delete periode 4 directory

--- periode 4/Python opdrachten/Opdracht_2/Opdracht_2.3/main.py
+++
@@ -1,6 +0,0 @@
-# Function: Shows the numbers from 0 to 100.
-# Author: Nick van der Ree
-
-# Range function allows the script to show a range of numbers.
-for num in range(0, 100, 10):
-    print(num)
