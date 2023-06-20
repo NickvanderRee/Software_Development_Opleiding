956bfcc3bfd7f3c2888f9a521b6e6b74bbe42ffd
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2023-06-20 10:54:30 +0200

    Delete Leerjaar_1/Periode_4 directory

--- Leerjaar_1/Periode_4/Python opdrachten/Opdracht_2/Opdracht_2.2/readme.md
+++
@@ -1,1 +0,0 @@
-Opdracht 2.2 is het zelfde als 2.1
--- Leerjaar_1/Periode_4/Python opdrachten/Opdracht_3/Opdracht_3.1/main.py
+++
@@ -1,9 +0,0 @@
-# Function: Checks if you are old enough to take driving lessons.
-# Author: Nick van der Ree
-
-leeftijd = int(input("Wat is je leeftijd? "))
-
-if leeftijd >= 18:
-    print("Gefeliciteerd! Je bent oud genoeg om rijlessen te nemen.")
-else:
-    print("Sorry, je bent nog niet oud genoeg om rijlessen te nemen.")
