72b127a35a9563cef06da1cc7cf98a216065e25d
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2023-06-20 10:38:52 +0200

    Delete leerjaar_1-periode_4 directory

--- leerjaar_1-periode_4/OOP_opdrachten/OOP_9 delen/OOP_9.2/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'Music.php';
-
-    $music1 = new Music (name: 'Bach', genre:'Klassiek', listen: 3);
-    
-    echo $music1->getName()."<br><br>";
-    
-    var_dump($music1);
-?>
